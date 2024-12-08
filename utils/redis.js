1c182b8a23bc7b7ecde709f6d7dfb2b8074a2c6f
Akinladejo <52078471+Akinladejo@users.noreply.github.com>
2024-12-08 20:29:51 +0100

    redis.js

---
+++ utils/redis.js
@@ -0,0 +1,41 @@
+import redis from 'redis';
+import { promisify } from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient({
+      host: process.env.REDIS_HOST || 'localhost',
+      port: process.env.REDIS_PORT || 6379,
+    });
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.expireAsync = promisify(this.client.expire).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+
+    this.client.on('error', (error) => {
+      console.error(`Redis client error: ${error}`);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return this.getAsync(key);
+  }
+
+  async set(key, value, duration) {
+    await this.setAsync(key, value);
+    if (duration) {
+      await this.expireAsync(key, duration);
+    }
+  }
+
+  async del(key) {
+    await this.delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
